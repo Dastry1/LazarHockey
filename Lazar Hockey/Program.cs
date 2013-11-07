ebaa92c7a0d5e18ac96121e34ab858e3e9a35b22
Dastry1 <david.biggs@hotmail.com>
2013-11-07 18:42:29 +0000

    First Commit

---
+++ Lazar Hockey/Program.cs
@@ -0,0 +1,29 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace Lazar_Hockey
+{
+    public static class Program
+    {
+        private static readonly Color CornflowerBlue = new Color(100, 149, 237);
+
+        public static void Main(string[] args)
+        {
+            RenderWindow window = new RenderWindow(new VideoMode(1280, 720), "SFML Window");
+            window.Closed += (sender, eventArgs) => window.Close();
+
+            while (window.IsOpen())
+            {
+                window.DispatchEvents();
+
+                // TODO: Insert Update Code Here
+
+                window.Clear(CornflowerBlue);
+
+                // TODO: Insert Draw Code Here
+
+                window.Display();
+            }
+        }
+    }
+}
